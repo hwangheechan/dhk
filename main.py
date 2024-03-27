05faee2a7c6154290ffbc20f19c24d8e3257ebcb
hwangheechan <163493858+hwangheechan@users.noreply.github.com>
2024-03-27 23:02:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 DongHyun Kim
 
+def max(a,b,c):
+  mx = None
+  if a>b:
+    if a>c:
+      mx = a
+    else:
+      mx = c
+  else:
+    if b>c:
+      mx=b
+    else:
+      mx=c
+  return mx
+
+x = int(input())
+y = int(input())
+z = int(input())
+
+m = max(x,y,z)
+print(m)
